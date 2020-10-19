67f191f4237659dba22b6d930ff7dc9f1e260f10
Maxim Shatskikh <shatskikhms@gmail.com>
2020-10-19 18:44:44 +0300

    базовое приложение телебот

---
+++ cmd/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+//точка входа программы
+func main() {
+	botToken := "1002130423:AAG1fk3_C4PSn3172SATn29jgtp5T7R5o60"
+	//https://api.telegram.org/bot<token>/METHOD_NAME
+	botApi := "https://api.telegram.org/bot"
+	botUrl := botApi + botToken
+	offset := 0
+
+	for {
+		updates, err := getUpdates(botUrl, offset)
+		if err != nil {
+			log.Println("Something go wrong: ", err.Error())
+		}
+
+		for _, update := range updates {
+			err = respond(botUrl, update)
+			offset = update.UpdateId + 1
+		}
+
+		fmt.Println(updates)
+	}
+}
+
+//запрос обновлений
+func getUpdates(botUrl string, offset int) ([]Update, error) {
+	resp, err := http.Get(botUrl + "/getUpdates" + "?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var restResponse RestResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return restResponse.Result, nil
+}
+
+//ответ на обновления
+func respond(botUrl string, update Update) error {
+	var botMessage BotMessage
+	botMessage.ChatId = update.Message.Chat.ChatId
+	botMessage.Text = update.Message.Text
+
+	buf, err := json.Marshal(botMessage)
+	if err != nil {
+		return err
+	}
+
+	_, err = http.Post(botUrl+"/sendMessage", "application/json", bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
